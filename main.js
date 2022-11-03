7cf66fddf3fe08cefa6c28bd6d56b43d712605cb
MarcoPerta <perta.marco@gmail.com>
2022-11-03 15:24:22 +0100

    numeri random nell'html

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ let array = [];
 console.log(array)
 // variabile singolo numero random
 let numeroRandom;
+// variabile per scrivere numeri array nell'html
+let numeriNellHtml = document.getElementById('numeriArray');
 
 // funzione per generare numeri random
 function get5Random(max,min){
@@ -14,8 +16,9 @@ function get5Random(max,min){
 }
 get5Random(100,1)
 
-// ciclo per immettere 5 num random nell'array
+// ciclo per immettere 5 num random nell'array e stamparli nell'html
 for( x=0; x<5; x++){
     numeroRandom = get5Random(100,1);
     array.push(numeroRandom);
+    numeriNellHtml.innerHTML += `${numeroRandom}`;
 }
