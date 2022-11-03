9119b6c0d19cfc44c0eda8670552e18db4ce4615
MarcoPerta <perta.marco@gmail.com>
2022-11-03 15:15:55 +0100

    5 numeri random in array

---
+++ main.js
@@ -0,0 +1,21 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// array vuoto
+let array = [];
+console.log(array)
+// variabile singolo numero random
+let numeroRandom;
+
+// funzione per generare numeri random
+function get5Random(max,min){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+get5Random(100,1)
+
+// ciclo per immettere 5 num random nell'array
+for( x=0; x<5; x++){
+    numeroRandom = get5Random(100,1);
+    array.push(numeroRandom);
+}
